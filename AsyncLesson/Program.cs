526c8c9fa3e58fb4df52b9e8c705baf2043f9349
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-12-11 19:44:49 +0600

    Создали делегат и запустили в главном потоке

--- AsyncLesson/Program.cs
+++ AsyncLesson/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading;
 
 namespace AsyncLesson
 {
     class Program
     {
+        //delegate int MyAction(); 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var action = new MyAction(CalculateSophisticNumber);
+            var action = new Func<int>(CalculateSophisticNumber);
+            //action();
+            var result = action.Invoke();
+            Console.WriteLine(result);
+            Console.WriteLine("Главный поток завершил работу");
+            Console.ReadLine();
+        }
+
+        private static int CalculateSophisticNumber()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - выполняет работу");
+            Thread.Sleep(5000);
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - закончил работу метода");
+            return 20000;
         }
     }
 }
